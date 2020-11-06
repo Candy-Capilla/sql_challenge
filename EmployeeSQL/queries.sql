12b1dccfaf30c85714359d88870b5bdb8efd04ba
Candy Capilla <candycapilla@MacBook-Pro-3.local>
2020-11-06 15:43:06 -0500

    final analysis

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -50,7 +50,10 @@ WHERE dept_name = 'Sales'
 OR dept_name = 'Development';
 
 ---descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-
+SELECT last_name, COUNT(last_name) AS "Frequency Count"
+FROM employees
+GROUP BY last_name
+ORDER BY "Frequency Count" DESC;
 
 ----BONUS
 from sqlalchemy import create_engine
